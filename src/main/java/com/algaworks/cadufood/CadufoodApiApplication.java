23db3b4d68548a22c08ea677e295753493db4a2b
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-12-05 22:35:06 -0300

    first commit

---
+++ src/main/java/com/algaworks/cadufood/CadufoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.cadufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadufoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadufoodApiApplication.class, args);
+	}
+
+}
