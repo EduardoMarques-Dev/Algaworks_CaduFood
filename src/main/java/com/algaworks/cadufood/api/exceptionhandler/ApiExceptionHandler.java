5b4ba60e648346998d956d62167c351944db1168
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2023-01-10 19:43:35 -0300

    feat: aula 8.20. Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/cadufood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/cadufood/api/exceptionhandler/ApiExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -36,13 +37,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                     .build();
         }
 
-
         return super.handleExceptionInternal(ex, body, headers, statusCode, request);
     }
 
+    // EXCEPTIONS DE NEGÓCIO
+
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<?> handleNegocioException(NegocioException ex, WebRequest request){
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+        HttpStatus statusCode = HttpStatus.BAD_REQUEST;
+        ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(statusCode, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), statusCode, request);
     }
 
     @ExceptionHandler({
@@ -59,13 +67,35 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         Problem problem = createProblemBuilder(statusCode, problemType, detail).build();
 
-        return handleExceptionInternal(ex, problem, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), statusCode, request);
     }
 
-    @ExceptionHandler({EntidadeEmUsoException.class})
+    @ExceptionHandler(EntidadeEmUsoException.class)
     public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeNaoEncontradaException ex, WebRequest request){
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+
+        HttpStatus statusCode = HttpStatus.CONFLICT;
+        ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+        String detail = ex.getMessage();
+
+        Problem problem = createProblemBuilder(statusCode, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), statusCode, request);
     }
+
+    // EXCEPTIONS SOBRESCRITAS
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = "O corpo da requisição está inválido. Verifique o erro de sintaxe.";
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
+
+    // UTILITÁRIOS
 
     private Problem.ProblemBuilder createProblemBuilder(HttpStatusCode statusCode, ProblemType problemType, String detail){
         return Problem.builder()
@@ -75,5 +105,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
                 .detail(detail)
                 .timestamp(LocalDateTime.now());
     }
+
 
 }
--- src/main/java/com/algaworks/cadufood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/cadufood/api/exceptionhandler/ProblemType.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
-    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada","Entidade não encontrada");
+    ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+    ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada","Entidade não encontrada"),
+    ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensível");
+
 
     private String title;
     private String uri;
