1bb9feeefd07123f107ca506eb871e5811530b72
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2023-01-10 21:01:20 -0300

    feat: aula 8.26. Desafio: tratando a exceção NoHandlerFoundException

--- src/main/java/com/algaworks/cadufood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/cadufood/api/exceptionhandler/ApiExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -134,6 +135,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
     }
 
+    // METHOD NOT ALLOWED EXCEPTION
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        ProblemType problemType = ProblemType.RECURSO_NAO_ENCONTRADO;
+        String detail = String.format("O recurso '%s' que você tentou acessar não existe.",
+                ex.getRequestURL());
+
+        Problem problem = createProblemBuilder(status, problemType, detail).build();
+
+        return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
+    }
 
     // UTILITÁRIOS
 
--- src/main/java/com/algaworks/cadufood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/cadufood/api/exceptionhandler/ProblemType.java
@@ -7,6 +7,7 @@ public enum ProblemType {
 
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada","Entidade não encontrada"),
+    RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado","Recurso não encontrado"),
     ENTIDADE_EM_USO("/entidade-em-uso","Entidade em uso"),
     PARAMETRO_INVALIDO("/parametro-invalido","Parâmetro inválido"),
     MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel","Mensagem incompreensível");
