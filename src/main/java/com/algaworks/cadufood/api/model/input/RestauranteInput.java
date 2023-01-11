196e8a9d216d0f226a66dd9b8cb867df9d813c82
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2023-01-11 13:01:06 -0300

    feat: aula 9.5. Conhecendo e adicionando mais constraints de validação no modelo

--- src/main/java/com/algaworks/cadufood/api/model/input/RestauranteInput.java
+++ src/main/java/com/algaworks/cadufood/api/model/input/RestauranteInput.java
@@ -3,7 +3,7 @@ package com.algaworks.cadufood.api.model.input;
 import com.algaworks.cadufood.domain.model.Cozinha;
 import com.algaworks.cadufood.domain.model.Endereco;
 import com.algaworks.cadufood.domain.model.util.GenericEntity;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,10 +16,13 @@ public class RestauranteInput implements GenericEntity<RestauranteInput> {
     @NotBlank
     private String nome;
 
+    @PositiveOrZero
     private BigDecimal taxaFrete;
 
+    @NotNull
     private Endereco endereco;
 
+    @NotNull
     private Cozinha cozinha;
 
 }
