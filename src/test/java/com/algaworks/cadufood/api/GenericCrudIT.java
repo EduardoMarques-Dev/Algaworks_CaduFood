3e9883f5185b39849315105af10a567111ddc23d
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2023-01-12 01:21:03 -0300

    feat: aula 10.10. Criando um método para fazer setup dos testes

--- src/test/java/com/algaworks/cadufood/api/GenericCrudIT.java
+++ src/test/java/com/algaworks/cadufood/api/GenericCrudIT.java
@@ -1,15 +1,20 @@
 package com.algaworks.cadufood.api;
 
 import io.restassured.http.ContentType;
+import org.aspectj.lang.annotation.Before;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,21 +23,32 @@ public class GenericCrudIT {
     @LocalServerPort
     private int port;
 
+    @BeforeEach
+    public void setUp(){
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port = port;
+        RestAssured.basePath = "/cozinhas";
+    }
+
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
-        // EXIBE O REQUEST E RESPONSE, CASO FALHE A REQUISIÇÃO
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        given()
+                .accept(ContentType.JSON)
+            .when()
+                .get()
+            .then()
+                .statusCode(HttpStatus.OK.value());
+    }
 
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
         RestAssured.given()
-                .basePath("/cozinhas")
-                .port(port)
                 .accept(ContentType.JSON)
             .when()
                 .get()
             .then()
                 .body("", Matchers.hasSize(2))
-                .body("nome", Matchers.hasItems("Indiana","Tailandesa"))
-                .statusCode(200);
+                .body("nome", Matchers.hasItems("Indiana","Tailandesa"));
     }
 
 }
