ea592207a7dc08cb45470c3c222fd4d9e4066b9e
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2023-01-13 14:34:57 -0300

    feat: 10.-- Implementando banco de teste

--- src/test/java/com/algaworks/cadufood/api/GenericCrudIT.java
+++ src/test/java/com/algaworks/cadufood/api/GenericCrudIT.java
@@ -1,54 +1,76 @@
 package com.algaworks.cadufood.api;
 
+import com.algaworks.cadufood.domain.model.Cozinha;
+import com.algaworks.cadufood.domain.repository.CozinhaRepository;
+import com.algaworks.cadufood.util.DatabaseCleaner;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.aspectj.lang.annotation.Before;
-import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.given;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class GenericCrudIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
 
     @BeforeEach
     public void setUp(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        databaseCleaner.clearTables();
+        prepararDados();
     }
 
     @Test
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .get()
-            .then()
+                .then()
                 .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+    public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
         RestAssured.given()
                 .accept(ContentType.JSON)
-            .when()
+                .when()
                 .get()
-            .then()
+                .then()
                 .body("", Matchers.hasSize(2))
                 .body("nome", Matchers.hasItems("Indiana","Tailandesa"));
+    }
+
+    private void prepararDados(){
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Indiana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha2);
     }
 
 }
